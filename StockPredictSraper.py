f8681312134b46e10984e2611c55b6ffa642280b
Bob <bob.sheets76@gmail.com>
2025-09-17 10:45:06 -0600

    Updating code to convert from JSON

--- StockPredictSraper.py
+++ StockPredictSraper.py
@@ -1,11 +1,15 @@
 # Import required modules
 import requests
 import csv
+import pandas as pd
+import json
 
 # url to download data
 url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=AMZN&apikey=QJZ37Q0YG6D3I1Y8'
 r = requests.get(url)
-data = r.csv()
+data = r.json()
 
-# print the data
-print(data)
+# Write to csv
+with open('c:/users/allpo/desktop/content/amzn_stock_data.csv', 'w', newline='') as amzn_stock_data:
+    writer = csv.writer(amzn_stock_data)
+    writer.writerows(data)
