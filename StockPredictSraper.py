987338579090e7956f8e0636f496b9ef2d805df4
Bob <bob.sheets76@gmail.com>
2025-09-18 09:59:55 -0600

    encrypted the URL

--- StockPredictSraper.py
+++ StockPredictSraper.py
@@ -3,11 +3,17 @@ import requests
 import csv
 import pandas as pd
 import json
+from cryptography.fernet import Fernet
+
+key = Fernet.generate_key()
+fernet = Fernet(key)
 
 # url to download data
 url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=AMZN&apikey=QJZ37Q0YG6D3I1Y8'
 r = requests.get(url)
 data = r.json()
+
+encrypted_url = fernet.encrypt(url)
 
 # Write to csv
 with open('c:/users/allpo/desktop/content/amzn_stock_data.csv', 'w', newline='') as amzn_stock_data:
