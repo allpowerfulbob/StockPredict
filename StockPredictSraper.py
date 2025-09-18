a4bc1ee23562ffd32f04148c56ce3a060c1b694a
Bob <bob.sheets76@gmail.com>
2025-09-18 10:16:56 -0600

    write data to json file, convert json file to csv

--- StockPredictSraper.py
+++ StockPredictSraper.py
@@ -3,17 +3,30 @@ import requests
 import csv
 import pandas as pd
 import json
-from cryptography.fernet import Fernet
 
-key = Fernet.generate_key()
-fernet = Fernet(key)
 
 # url to download data
 url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=AMZN&apikey=QJZ37Q0YG6D3I1Y8'
 r = requests.get(url)
 data = r.json()
 
-# Write to csv
-with open('c:/users/allpo/desktop/content/amzn_stock_data.csv', 'w', newline='') as amzn_stock_data:
-    writer = csv.writer(amzn_stock_data)
-    writer.writerows(data)
+# Dump data to a json file
+with open("c:/users/allpo/desktop/content/amzn_stock_data.json", "w") as f:
+    json.dump(data, f)
+
+# Transfer json file to csv
+with open("c:/users/allpo/desktop/content/amzn_stock_data.json") as jf:
+    jd = json.load(jf)
+
+df = open('c:/users/allpo/desktop/content/amzn_stock_data.csv', 'w', newline='')
+cw = csv.writer(df)
+
+c = 0
+for data in jd:
+    if c == 0:
+        header = data.keys()
+        cw.writerow(header)
+        c += 1
+        cw.writerow(data.values())
+
+df.close
