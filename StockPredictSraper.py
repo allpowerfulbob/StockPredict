8063c01481cad12ece3917100cc914db41dccd39
Bob <bob.sheets76@gmail.com>
2025-09-19 10:12:10 -0600

    Continuing to troubleshoot errors in writing

--- StockPredictSraper.py
+++ StockPredictSraper.py
@@ -8,10 +8,9 @@ import json
 # url to download data, replace the API Key with your own
 url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=AMZN&interval=60min&apikey=QJZ37Q0YG6D3I1Y&datatype=csv'
 r = requests.get(url)
+data = open('c:/users/allpo/desktop/content/amzn_stock_data.csv', 'w', newline='')
 
-df = open('c:/users/allpo/desktop/content/amzn_stock_data.csv', 'w', newline='')
-fieldnames = ['Open', 'High', 'Low', 'Close', 'Volume']
-cw = csv.Writer(df, fieldnames=fieldnames)
+cw = csv.Writer(data)
 cw.writeheader()
 
 c = 0
