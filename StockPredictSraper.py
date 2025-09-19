46c6eb1b3daf1f72b163ddc8d955a71358710f44
Bob <bob.sheets76@gmail.com>
2025-09-19 09:53:12 -0600

    Corrected url to pull the daily and historical OHLCV

--- StockPredictSraper.py
+++ StockPredictSraper.py
@@ -6,8 +6,7 @@ import json
 
 
 # url to download data, replace the API Key with your own
-url = 'https://www.alphavantage.co/query?function=HISTORICAL_OPTIONS&symbol=AMZN&date=2017-11-15&apikey=QJZ37Q0YG6D3I1Y&datatype=csv'
-url = 'https://www.alphavantage.co/query?function=ANALYTICS_FIXED_WINDOW&SYMBOLS=AMZN&RANGE=full&INTERVAL=DAILY&OHLC=close&CALCULATIONS=CORRELATION&apikey=QJZ37Q0YG6D3I1Y&datatype=csv'
+url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=AMZN&interval=5min&apikey=QJZ37Q0YG6D3I1Y&datatype=csv'
 r = requests.get(url)
 data = pd.read_csv('c:/users/allpo/desktop/content/amzn_stock_data.csv')
 
