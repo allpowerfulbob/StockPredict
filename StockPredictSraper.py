eb2c2ff66514f1884ea30236a10d5a61bfa8bab5
Bob <bob.sheets76@gmail.com>
2025-09-19 10:10:08 -0600

    Corrected code to write correctly take 2

--- StockPredictSraper.py
+++ StockPredictSraper.py
@@ -8,7 +8,6 @@ import json
 # url to download data, replace the API Key with your own
 url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=AMZN&interval=60min&apikey=QJZ37Q0YG6D3I1Y&datatype=csv'
 r = requests.get(url)
-data = pd.read_csv('c:/users/allpo/desktop/content/amzn_stock_data.csv')
 
 df = open('c:/users/allpo/desktop/content/amzn_stock_data.csv', 'w', newline='')
 fieldnames = ['Open', 'High', 'Low', 'Close', 'Volume']
