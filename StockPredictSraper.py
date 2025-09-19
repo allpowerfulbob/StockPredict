b11f29057a6e5f4ea4dfaf1419dec50ef91e3ac1
Bob <bob.sheets76@gmail.com>
2025-09-19 10:07:57 -0600

    Corrected code for proper writing

--- StockPredictSraper.py
+++ StockPredictSraper.py
@@ -9,7 +9,18 @@ import json
 url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=AMZN&interval=60min&apikey=QJZ37Q0YG6D3I1Y&datatype=csv'
 r = requests.get(url)
 data = pd.read_csv('c:/users/allpo/desktop/content/amzn_stock_data.csv')
-dfr = pd.DataFrame
-dfr.to_csv('c:/users/allpo/desktop/content/amzn_stock_data.csv')
 
+df = open('c:/users/allpo/desktop/content/amzn_stock_data.csv', 'w', newline='')
+fieldnames = ['Open', 'High', 'Low', 'Close', 'Volume']
+cw = csv.Writer(df, fieldnames=fieldnames)
+cw.writeheader()
 
+c = 0
+for stock_data in data:
+    if c == 0:
+        header = stock_data.keys()
+        cw.writerow(header)
+        c += 1
+        cw.writerow(stock_data.values())
+
+df.close
