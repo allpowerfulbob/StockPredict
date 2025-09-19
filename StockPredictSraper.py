708906ecbd92d54a5cb0d0e1c06d24660ce24a67
Bob <bob.sheets76@gmail.com>
2025-09-19 10:04:37 -0600

    removed possible duplicate code

--- StockPredictSraper.py
+++ StockPredictSraper.py
@@ -6,21 +6,10 @@ import json
 
 
 # url to download data, replace the API Key with your own
-url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=AMZN&interval=5min&apikey=QJZ37Q0YG6D3I1Y&datatype=csv'
+url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=AMZN&interval=60min&apikey=QJZ37Q0YG6D3I1Y&datatype=csv'
 r = requests.get(url)
 data = pd.read_csv('c:/users/allpo/desktop/content/amzn_stock_data.csv')
+dfr = pd.DataFrame
+dfr.to_csv('c:/users/allpo/desktop/content/amzn_stock_data.csv')
 
-df = open('c:/users/allpo/desktop/content/amzn_stock_data.csv', 'w', newline='')
-fieldnames = ['Open', 'High', 'Low', 'Close', 'Volume']
-cw = csv.DictWriter(df, fieldnames=fieldnames)
-cw.writeheader()
 
-c = 0
-for stock_data in data:
-    if c == 0:
-        header = stock_data.keys()
-        cw.writerow(header)
-        c += 1
-        cw.writerow(stock_data.values())
-
-df.close
