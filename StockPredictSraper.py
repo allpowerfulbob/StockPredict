e32bc2b43781ea6d0a60d8264ffb9dc19502a91b
Bob <bob.sheets76@gmail.com>
2025-09-22 08:48:56 -0600

    Trying to piece together a final url with a first and second part

--- StockPredictSraper.py
+++ StockPredictSraper.py
@@ -10,7 +10,9 @@ params = input("What stock would you like to get data for?")
 
 # Use urllib to amend the url with params
 passed_params = urllib.parse.urlencode(params)
-final_url = f'{https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=}?{passed_params}&interval=60min&apikey=QJZ37Q0YG6D3I1Y&datatype=csv'
+first_part_final_url = f'{https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=}'{passed_params}
+second_part_final_url = f'&interval=60min&apikey=QJZ37Q0YG6D3I1Y&datatype=csv'
+final_url = (first_part_final_url) + passed_params + (second_part_final_url)
 # url to download data, replace the API Key with your own
 r = requests.get(final_url)
 data = open('c:/users/allpo/desktop/content/amzn_stock_data.csv', 'w', newline='')
