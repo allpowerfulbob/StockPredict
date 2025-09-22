0c22fbebe28e0f5c4fb7afa6f9f45e87ecd3238d
Bob <bob.sheets76@gmail.com>
2025-09-22 08:53:36 -0600

    converted both urls to strings

--- StockPredictSraper.py
+++ StockPredictSraper.py
@@ -10,8 +10,8 @@ params = input("What stock would you like to get data for?")
 
 # Use urllib to amend the url with params
 passed_params = urllib.parse.urlencode(params)
-first_part_final_url = f'{https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=}'
-second_part_final_url = f'&interval=60min&apikey=QJZ37Q0YG6D3I1Y&datatype=csv'
+first_part_final_url = str('{https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=}')
+second_part_final_url = str(f'&interval=60min&apikey=QJZ37Q0YG6D3I1Y&datatype=csv')
 final_url = (first_part_final_url) + (passed_params) + (second_part_final_url)
 # url to download data, replace the API Key with your own
 r = requests.get(final_url)
